3df03708a23e55bfc9f28b981fe45c9b80d09923
MomingCoder <a398445075@gmail.com>
2016-05-31 21:35:55 +0800

    geturl

--- get_information.py
+++ get_information.py
@@ -1,1 +1,13 @@
 __author__ = 'Moming'
+
+import urllib
+from BeautifulSoup import *
+import re
+import requests
+
+
+
+for i in range(10):
+    url = 'https://movie.douban.com/top250?start=0' + str(i * 25) + '&filter='
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
